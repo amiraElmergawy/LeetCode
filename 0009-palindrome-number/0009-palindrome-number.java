852718a1743db11c12ad4e2a38fdb5f0358795cf
Amira Elmergawy <49119157+amiraElmergawy@users.noreply.github.com>
2023-09-05 10:13:05 +0300

    Time: 8 ms (41.97%), Space: 42.5 MB (92.33%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
+        if(x < 0) return false;
+        if(x < 10) return true;
         int right, left;
         int rightRemainder = x;
         int leftRemainder = x;
-        
-        int leftRemainderSize = String.valueOf(Math.abs(leftRemainder)).length();
-        if(x < 0) return false;
-        while(leftRemainderSize != 0){
+        //Math.log10(n)+1
+        int leftRemainderSize = (int)Math.log10(leftRemainder)+1;
+        final int size = leftRemainderSize/2;
+        while(leftRemainderSize != size){
               right = rightRemainder % 10;
               rightRemainder = rightRemainder / 10;
               left = (int)(leftRemainder / Math.pow(10, leftRemainderSize-1));
